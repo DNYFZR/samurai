932ab5ed65da081f9ba521bd426d53a4fb148252
Danny <76977155+DNYFZR@users.noreply.github.com>
2025-03-28 18:39:45 +0000

    initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+<h2 align="center">Samurai</h2>
+
+Toying around with an ai-chat application :
+
+- Built with TS & React
+- Uses Hugging Face Transformers.js package 
+- Renders output with React Markdown package
---
+++ src/main.tsx
@@ -0,0 +1,67 @@
+import "./index.css";
+import icon from "/icon.png";
+import { StrictMode, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import { pipeline } from "@huggingface/transformers";
+
+import Markdown from "react-markdown"; 
+import rehypeRaw from "rehype-raw";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from 'remark-gfm';
+import rehypeHighlight from "rehype-highlight";
+
+function App() {
+  const [userInput, setUserInput] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
+  const maxTokens = 2000;
+
+  const promptAI = async() => {
+    setResponse("Processing...");
+
+    const messages = [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: userInput },
+    ];
+
+    const model = await pipeline(
+      "text-generation",
+      "Mozilla/Qwen2.5-0.5B-Instruct",
+      { dtype: "q8" },
+    );
+    
+    let result = await model(
+      messages, 
+      { max_new_tokens: maxTokens, do_sample:false },
+    );
+    
+    const text:string = Object(result)[0].generated_text.at(-1).content;
+    setResponse(text);
+    setUserInput("");
+  };
+  
+  return(
+    <div>
+      <img className="icon" src={icon}/>
+
+      <div className="output">
+        <Markdown 
+          children={response}
+          remarkPlugins={[remarkGfm]} 
+          rehypePlugins={[rehypeRaw, rehypeSlug, rehypeHighlight]}
+        />
+      </div>
+      
+      <div className="row">
+        <textarea className="userInput" placeholder="Ask me anything..." onChange={(e) => setUserInput(e.target.value)}/>
+        <button onClick={promptAI}>Send</button>
+      </div>
+
+    </div>
+  );
+};
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
